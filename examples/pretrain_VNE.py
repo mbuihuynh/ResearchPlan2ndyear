ee92972db20e92e602246067c7b5ccfd61cb4fcc
Mai.Bui <buihuynhmai@gmail.com>
2025-02-07 07:18:03 +0700

    Create pretrain_VNE.py

---
+++ examples/pretrain_VNE.py
@@ -0,0 +1,13 @@
+""" Experiments LLM on the only VNE language for multi-tasks
+    - Task1: Sentiment Classification Task
+    - Task2: Named Entity Recognition
+    - Task3: Q&A
+    - Task4: Text Generation
+    - Task5: Asia Language
+
+
+
+Reference:
+    - https://github.com/vndee/awsome-vietnamese-nlp
+
+"""
